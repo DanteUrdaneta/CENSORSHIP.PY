61490bb4be087e5b3951594d12170e5d65ac085c
DanteUrdaneta <danteurd@gmail.com>
2024-10-08 17:48:51 -0400

    setup and requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+  description = f.read()
+
 setup(
-  name="censorship.py",
-  version="0.1",
-  packages=find_packages(),
+  name = "censorship.py",
+  version = "0.1",
+  packages= find_packages(),
   include_package_data=True,
   install_requires=[
     'pydub',
     'sentence-transformers',
-    'faster-whisper',	]
+    'faster-whisper',
+  ],
+  long_description=description,
+  long_description_content_type="text/markdown",
+  author="NestorDante",
+  author_email="nestor.d.urdaneta@gmail.com", 
 )
